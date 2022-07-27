810406356df5e6ffa8717e557b0f6f6fa1e97fed
crissegura00 <crisemanuelsegura@gmail.com>
2022-07-27 17:56:18 -0300

    .

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let months = ['January','February','March','April','May','June','July','August',
 
 dayString.textContent =week[date.getDay()]
 
-dayNumber.textContent = (date.getDate()+' / '+months[date.getMonth()]+' / '+date.getFullYear())
+dayNumber.textContent = (months[date.getMonth()]+' / '+date.getDate()+' / '+date.getFullYear())
 
 
 //newTask
@@ -15,6 +15,8 @@ let newTask = document.getElementById('newTask')
 let add = document.getElementById('add')
 let list = document.getElementById('list')
 let clear = document.getElementById('clear')
+let tasks = []
+
 //functionAddTask
 function addTask(){
     item = newTask.value
@@ -26,7 +28,6 @@ function addTask(){
         
         //createDiscard
         let discard=document.createElement('button')
-        discard.textContent = '×'
         discard.onclick=()=>{
             list.removeChild(li)
             nothingDisplay()
@@ -34,16 +35,19 @@ function addTask(){
         //append
         list.append(li)
         li.append(discard)
+        tasks.push(li.textContent)
 
         //clearInput
         newTask.value = ''
+
+        //localStorageSetItem
+        localStorage.setItem('tasks',JSON.stringify(tasks))
 
     }else{
         alert('Please, add a new task.')
     }
 }
-
-//nothing
+//functionNothingDisplay
 function nothingDisplay(){
     let nothing = document.getElementById('nothing')
     let lis = document.querySelectorAll('li')
@@ -59,8 +63,7 @@ add.onclick=(e)=>{
     addTask()
     nothingDisplay()
 }
-
-
+//Clear
 clear.onclick=()=>{
     let lis = document.querySelectorAll('li')
     if(lis.length!==0){
@@ -71,5 +74,23 @@ clear.onclick=()=>{
         alert("You don't have any pending task.")
     }            
 }
+//functionLocalStorageGetItem
+function getItem(){
+    let saved = JSON.parse(localStorage.getItem('tasks'))
+    
+    while (saved.length >0){
+    let discard=document.createElement('button')
+    discard.onclick=()=>{
+        list.removeChild(li)
+        nothingDisplay()
+    }
 
+    li = document.createElement('li')
+    li.textContent = saved.pop()
+    li.append(discard)
+    list.append(li)
+    }
+}
+
+getItem()
 
