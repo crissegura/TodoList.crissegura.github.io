41ec11282ae66853e1cd0243b6367e8202f8b5af
crissegura00 <crisemanuelsegura@gmail.com>
2022-08-10 17:21:57 -0300

    mode

--- js/main.js
+++ js/main.js
@@ -1,29 +1,12 @@
-//modeLigthOrDark
-let ligthMode = document.getElementById('ligthMode')
-let darkMode = document.getElementById('darkMode')
+//mode
+const mode = document.getElementById('mode')
 
-ligthMode.onclick = ()=>{
-    ligthModeF()
-}
-darkMode.onclick = ()=>{
-    darkModeF()
+mode.onclick = ()=>{
+    document.body.classList.toggle('dark')
+    mode.classList.toggle('active')
 }
 
-function ligthModeF(){
-    let body = document.getElementById('body')
-    body.style.backgroundColor='white'
-    body.style.color='black'
-    ligthMode.style.display='none'
-    darkMode.style.display='block'
-}
 
-function darkModeF(){
-    let body = document.getElementById('body')
-    body.style.backgroundColor='rgb(7, 6, 6)'
-    body.style.color='white'
-    ligthMode.style.display='block'
-    darkMode.style.display='none'
-}
 
 //date
 let dayString = document.getElementById('dayOfWeek')
